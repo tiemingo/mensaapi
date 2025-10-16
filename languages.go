7725cef182383480a672f9ec70d9abb3646e91bc
tiemingo <212736953+tiemingo@users.noreply.github.com>
2025-10-16 15:31:46 +0200

    feat: added support for english + removed exit call on error

---
+++ languages.go
@@ -0,0 +1,8 @@
+package mensaapi
+
+type Language string
+
+const (
+	LANGUAGE_GERMAN  Language = "de" // default
+	LANGUAGE_ENGLISH Language = "en"
+)
--- locations.go
+++ locations.go
@@ -1,50 +1,50 @@
 package mensaapi
 
-type LOCATION int
-type FACILITY int
+type Location int
+type Facility int
 
 const (
-	UNI_ULM    FACILITY = 1 // Universität Ulm
-	THU        FACILITY = 2 // Technische Hochschule Ulm
-	HS_AALEN   FACILITY = 3 // Hochschule Aalen
-	PAED_HS_SG FACILITY = 4 // Pädagogische Hochschule Schwäbisch Gmünd
-	HS_GEST_SG FACILITY = 5 // Hochschule für Gestaltung Schwäbisch Gmünd
-	HBC_BIB    FACILITY = 6 // HBC. Biberach
-	DHS_HDH    FACILITY = 7 // Duale Hochschule Heidenheim
+	FACILITY_UNI_ULM    Facility = 1 // Universität Ulm
+	FACILITY_THU        Facility = 2 // Technische Hochschule Ulm
+	FACILITY_HS_AALEN   Facility = 3 // Hochschule Aalen
+	FACILITY_PAED_HS_SG Facility = 4 // Pädagogische Hochschule Schwäbisch Gmünd
+	FACILITY_HS_GEST_SG Facility = 5 // Hochschule für Gestaltung Schwäbisch Gmünd
+	FACILITY_HBC_BIB    Facility = 6 // HBC. Biberach
+	FACILITY_DHS_HDH    Facility = 7 // Duale Hochschule Heidenheim
 )
 
 const (
 	// Universität Ulm
-	LOC_MENSA_UNI_ULM        LOCATION = 1  // Mensa in der Uni Ulm
-	LOC_CAF_HELMHOLZ_UNI_ULM LOCATION = 16 // Cafeteria Helmholtzstraße
-	LOC_CAF_WEST_UNI_ULM     LOCATION = 2  // Cafeteria West
-	// LOC_CAF_SOUTH_UNI_ULM          =    // Cafeteria Southside - Eingang Süd
-	// LOC_CAF_NORD_UNI_ULM           =    // Cafeteria Nord
-	// LOC_AUT_BIB_UNI_ULM            =    // Automatencafeteria in der Bibliothek
-	// LOC_CAF_TTU_UNI_ULM            =    // Cafeteria TTU
-	// LOC_WESTSIDE_UNI_ULM           =    // WestSide Diner
-	// LOC_CAMPUS_UNI_ULM             =    // Campus Diner
+	LOCATION_MENSA_UNI_ULM        Location = 1  // Mensa in der Uni Ulm
+	LOCATION_CAF_HELMHOLZ_UNI_ULM Location = 16 // Cafeteria Helmholtzstraße
+	LOCATION_CAF_WEST_UNI_ULM     Location = 2  // Cafeteria West
+	// LOCATION_CAF_SOUTH_UNI_ULM          =    // Cafeteria Southside - Eingang Süd
+	// LOCATION_CAF_NORD_UNI_ULM           =    // Cafeteria Nord
+	// LOCATION_AUT_BIB_UNI_ULM            =    // Automatencafeteria in der Bibliothek
+	// LOCATION_CAF_TTU_UNI_ULM            =    // Cafeteria TTU
+	// LOCATION_WESTSIDE_UNI_ULM           =    // WestSide Diner
+	// LOCATION_CAMPUS_UNI_ULM             =    // Campus Diner
 
 	// Technische Hochschule Ulm
-	LOC_MENSA_THU          LOCATION = 3  // THU Ulm Mensa
-	LOC_ESELSBERG_AUSG_THU LOCATION = 10 // THU Ulm Außenstelle Oberer Eselsberg Essensausgabe
-	// LOC_CAF_THU                  =    // THU Ulm Cafeteria
-	// LOC_CAF_ESELSBERG_THU        =    // THU Ulm Außenstelle Oberer Eselsberg Cafeteria Kiwi
+	LOCATION_MENSA_THU          Location = 3  // THU Ulm Mensa
+	LOCATION_ESELSBERG_AUSG_THU Location = 10 // THU Ulm Außenstelle Oberer Eselsberg Essensausgabe
+	// LOCATION_CAF_THU                  =    // THU Ulm Cafeteria
+	// LOCATION_CAF_ESELSBERG_THU        =    // THU Ulm Außenstelle Oberer Eselsberg Cafeteria Kiwi
 
 	// Hochschule Aalen
-	LOC_MENSA_HS_AALEN      LOCATION = 7 // Mensa
-	LOC_CAF_BURREN_HS_AALEN LOCATION = 5 // Cafeteria in der Hochschule
-	// LOC_CAF_HOCH_HS_AALEN         =   // Cafeteria in der Hochschule
+	LOCATION_MENSA_HS_AALEN      Location = 7 // Mensa
+	LOCATION_CAF_BURREN_HS_AALEN Location = 5 // Cafeteria in der Hochschule
+	// LOCATION_CAF_HOCH_HS_AALEN         =   // Cafeteria in der Hochschule
 
 	// Pädagogische Hochschule Schwäbisch Gmünd
-	LOC_MENSA_PAED_HS_SG LOCATION = 4 // Mensaria PH
+	LOCATION_MENSA_PAED_HS_SG Location = 4 // Mensaria PH
 
 	//  Hochschule für Gestaltung Schwäbisch Gmünd
-	LOC_HS_GEST_SG LOCATION = 12 // Hochschule für Gestaltung Schwäbisch Gmünd
+	LOCATION_HS_GEST_SG Location = 12 // Hochschule für Gestaltung Schwäbisch Gmünd
 
 	// HBC. Biberach
-	LOC_HBC_BIB LOCATION = 6 // HBC. Biberach
+	LOCATION_HBC_BIB Location = 6 // HBC. Biberach
 
 	// Duale Hochschule Heidenheim
-	LOC_DHS_HDH LOCATION = 15 // Duale Hochschule Heidenheim
+	LOCATION_DHS_HDH Location = 15 // Duale Hochschule Heidenheim
 )
